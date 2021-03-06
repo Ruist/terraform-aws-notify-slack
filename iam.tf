be237f17535d326dac63f4b4c371b5d35a72e1d6
Jeremiah Jinno <jj@airkit.com>
2021-03-05 17:14:41 -0800

    backporting upstream fixes - biggest fix is moving to using Lambda module instead of self-declaring Lambda resources - IAM resources now defined via looping over local variables - added variables/outputs to support new module use - tested against infra/global/regions/ids/ec2-alarms.tf (termination notifications) in staging

--- iam.tf
+++ iam.tf
@@ -15,55 +15,9 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-data "aws_iam_policy_document" "lambda_basic" {
-  count = (var.create == true ? 1 : 0)
-
-  statement {
-    sid = "AllowWriteToCloudwatchLogs"
-
-    effect = "Allow"
-
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-    ]
-
-    resources = [
-      "arn:aws:logs:*:*:*"]
-  }
-}
-
-data "aws_iam_policy_document" "lambda" {
-  count = var.create_with_kms_key == 1 ? 1 : 0 * (var.create == true ? 1 : 0)
-
-  source_json = data.aws_iam_policy_document.lambda_basic[0].json
-
-  statement {
-    sid = "AllowKMSDecrypt"
-
-    effect = "Allow"
-
-    actions = [
-      "kms:Decrypt"]
-
-    resources = [
-      var.kms_key_arn == "" ? "" : var.kms_key_arn]
-  }
-}
-
 resource "aws_iam_role" "lambda" {
   count = (var.create == true ? 1 : 0)
 
   name_prefix = "lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role[0].json
-}
-
-resource "aws_iam_role_policy" "lambda" {
-  count = (var.create == true ? 1 : 0)
-
-  name_prefix = "lambda-policy-"
-  role = aws_iam_role.lambda[0].id
-
-  policy = element(compact(concat(data.aws_iam_policy_document.lambda.*.json, data.aws_iam_policy_document.lambda_basic.*.json)), 0)
 }
--- main.tf
+++ main.tf
@@ -4,6 +4,12 @@ data "aws_sns_topic" "this" {
   name = var.sns_topic_name
 }
 
+resource "aws_cloudwatch_log_group" "lambda" {
+  count = (var.create == true ? 1 : 0)
+  name = "/aws/lambda/${var.lambda_function_name}"
+  retention_in_days = 30
+}
+
 resource "aws_sns_topic" "this" {
   count = (var.create_sns_topic == true ? 1 : 0) * (var.create == true ? 1 : 0)
 
@@ -13,80 +19,114 @@ resource "aws_sns_topic" "this" {
 locals {
   sns_topic_arn = element(concat(aws_sns_topic.this.*.arn, data.aws_sns_topic.this.*.arn, [
     ""]), 0)
+
+  lambda_policy_document = {
+    sid       = "AllowWriteToCloudwatchLogs"
+    effect    = "Allow"
+    actions   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
+    resources = [replace("${element(concat(aws_cloudwatch_log_group.lambda[*].arn, list("")), 0)}:*", ":*:*", ":*")]
+  }
+
+  lambda_policy_document_kms = {
+    sid       = "AllowKMSDecrypt"
+    effect    = "Allow"
+    actions   = ["kms:Decrypt"]
+    resources = [var.kms_key_arn]
+  }
+}
+
+data "aws_iam_policy_document" "lambda" {
+  count = var.create ? 1 : 0
+
+  dynamic "statement" {
+    for_each = concat([local.lambda_policy_document], var.kms_key_arn != "" ? [local.lambda_policy_document_kms] : [])
+    content {
+      sid       = statement.value.sid
+      effect    = statement.value.effect
+      actions   = statement.value.actions
+      resources = statement.value.resources
+    }
+  }
 }
 
 resource "aws_sns_topic_subscription" "sns_notify_slack" {
   count = (var.create == true ? 1 : 0)
 
-  topic_arn = local.sns_topic_arn
-  protocol = "lambda"
-  endpoint = aws_lambda_function.notify_slack[0].arn
-}
-
-resource "aws_lambda_permission" "sns_notify_slack" {
-  count = (var.create == true ? 1 : 0)
-
-  statement_id = "AllowExecutionFromSNS"
-  action = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.notify_slack[0].function_name
-  principal = "sns.amazonaws.com"
-  source_arn = local.sns_topic_arn
-}
-
-data "null_data_source" "lambda_file" {
-  inputs = {
-    filename = "${path.module}/functions/notify_slack.py"
-  }
-}
-
-data "null_data_source" "lambda_archive" {
-  inputs = {
-    filename = "${path.module}/functions/notify_slack.zip"
-  }
+  topic_arn     = local.sns_topic_arn
+  protocol      = "lambda"
+  endpoint      = module.lambda.this_lambda_function_arn
+  filter_policy = var.subscription_filter_policy
 }
 
 data "archive_file" "notify_slack" {
   count = (var.create == true ? 1 : 0)
 
   type = "zip"
-  source_file = data.null_data_source.lambda_file.outputs.filename
-  output_path = data.null_data_source.lambda_archive.outputs.filename
+  output_path = "${path.module}/files/dotfiles.zip"
+  source {
+    content = file("${path.module}/functions/notify_slack.py")
+    filename = "notify_slack.py"
+  }
 }
 
-resource "aws_lambda_function" "notify_slack" {
-  count = (var.create == true ? 1 : 0)
+module "lambda" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "1.28.0"
 
-  filename = data.archive_file.notify_slack[0].output_path
+  create = var.create
 
   function_name = var.lambda_function_name
+  description   = var.lambda_description
 
-  role = aws_iam_role.lambda[0].arn
-  handler = "notify_slack.lambda_handler"
-  source_code_hash = data.archive_file.notify_slack[0].output_base64sha256
-  runtime = "python3.7"
-  timeout = 30
-  kms_key_arn = var.kms_key_arn
+  handler                        = "notify_slack.lambda_handler"
+  source_path                    = "${path.module}/functions/notify_slack.py"
+  runtime                        = "python3.8"
+  timeout                        = 30
+  kms_key_arn                    = var.kms_key_arn
+  reserved_concurrent_executions = var.reserved_concurrent_executions
 
-  environment {
-    variables = {
-      SLACK_WEBHOOK_URL = var.slack_webhook_url
-      SLACK_CHANNEL = var.slack_channel
-      SLACK_USERNAME = var.slack_username
-      SLACK_EMOJI = var.slack_emoji
-      ENVIRONMENT = var.environment
+  # If publish is disabled, there will be "Error adding new Lambda Permission for notify_slack: InvalidParameterValueException: We currently do not support adding policies for $LATEST."
+  publish = true
+
+  environment_variables = {
+    SLACK_WEBHOOK_URL = var.slack_webhook_url
+    SLACK_CHANNEL     = var.slack_channel
+    SLACK_USERNAME    = var.slack_username
+    SLACK_EMOJI       = var.slack_emoji
+    LOG_EVENTS        = var.log_events ? "True" : "False"
+    ENVIRONMENT       = var.environment
+  }
+
+  lambda_role               = aws_iam_role.lambda[0].arn
+  role_name                 = "${var.iam_role_name_prefix}-${var.lambda_function_name}"
+  role_permissions_boundary = var.iam_role_boundary_policy_arn
+  role_tags                 = var.iam_role_tags
+
+  # Do not use Lambda's policy for cloudwatch logs, because we have to add a policy
+  # for KMS conditionally. This way attach_policy_json is always true independenty of
+  # the value of presense of KMS's famous "computed values in count" bug...
+  attach_cloudwatch_logs_policy = false
+  attach_policy_json            = true
+  policy_json                   = element(concat(data.aws_iam_policy_document.lambda[*].json, [""]), 0)
+
+  use_existing_cloudwatch_log_group = true
+  attach_network_policy             = var.lambda_function_vpc_subnet_ids != null
+
+  allowed_triggers = {
+    AllowExecutionFromSNS = {
+      principal  = "sns.amazonaws.com"
+      source_arn = local.sns_topic_arn
     }
   }
 
-  lifecycle {
-    ignore_changes = [
-      filename,
-      last_modified,
-    ]
-  }
+  store_on_s3 = var.lambda_function_store_on_s3
+  s3_bucket   = var.lambda_function_s3_bucket
+
+  vpc_subnet_ids         = var.lambda_function_vpc_subnet_ids
+  vpc_security_group_ids = var.lambda_function_vpc_security_group_ids
+
+  tags = merge(var.tags, var.lambda_function_tags)
+
+  depends_on = [aws_cloudwatch_log_group.lambda]
 }
 
-resource "aws_cloudwatch_log_group" "lambda_logs" {
-  count = (var.create == true ? 1 : 0)
-  name = "/aws/lambda/${var.lambda_function_name}"
-  retention_in_days = 30
-}
